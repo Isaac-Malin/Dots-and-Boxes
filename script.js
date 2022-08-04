aae4d1eec5a9aa27d1230ae54e1e0976b885786f
Isaac Malin <isaac.malin@mykenzie.snhu.edu>
2022-08-04 11:25:10 -0600

    I got the dots to line up with the borders. Need to get borders to change color on click of button

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// 
+// STEP 1
+
+// first team would {select the button/dot} at where they would like to start the line and select a second button/dot Adjacent but NOT diagnol to
+// the first button to creat a line inbetween the selected dots
+//on click add a green or blue border
+
+// Second team would {select the button/dot} at where they would like to start the line and select a second button/dot Adjacent but NOT diagnol to
+// the first button to creat a line inbetween the selected dots
+
+// STEP 2
+
+// repeat step 1 until the 
+
+const dots = document.querySelectorAll(".btn")
+
+dots.forEach((button) => {
+    button.addEventListener("click", () => {
+        button.classList.add(".dot").style.borderColor = "blue";
+    })
+})
